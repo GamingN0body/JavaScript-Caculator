3b13d6d886b65eab2d4fa6c36998ae4fa62ffcda
HLoghan <LegoSaber06@gmail.com>
2024-01-24 14:50:28 -0800

    wh

--- calcalator.js
+++ calcalator.js
@@ -1,1 +1,67 @@
 //node extension pack and code runner
+let num = Number;
+function visuals() {
+    let text;
+    let n1 = prompt("1 2 3");
+    let n2 = prompt("4 5 6");
+    let n3 = prompt("7 8 9");
+    let n4 = prompt("0 + -");
+    let n5 = prompt("x / =");
+    if (dontknow) {
+        document.write(n1, n2, n3, n4, n5);
+    }
+}
+
+let input = prompt("Please enter a caculation here.");
+function add(x, num) {
+        let input = prompt(x-num)
+        let output = document.write(x - num); 
+        
+        {document.write(add(x,num));}
+}
+
+//https://www.geeksforgeeks.org/javascript-program-to-make-a-simple-calculator/
+
+function minus(n, x) {
+    if (x === undefined) 
+    { return -x; }
+    else { return x - num; }
+}
+
+
+
+function multi(num) {
+    let input = promt("h/k=x")
+}
+
+
+function div(num) {
+    let input = promt(num + num)
+}
+
+
+function equal(num) {
+
+}
+
+
+function ifNeeded() {
+
+}
+
+
+console.log(visuals);
+console.log(minus);
+console.log(minus);
+console.log(multi);
+console.log(div);
+console.log(equal);
+console.log(ifNeeded);
+
+/*
+document.write(add);
+document.write(sub);
+document.write(multi);
+document.write(div);
+document.write(equal);
+document.write(ifNeeded);*/
