9923635753fec54c1a9293d913824b9bc39f506e
HLoghnan <143848580+GamingN0body@users.noreply.github.com>
2024-01-23 17:25:16 -0800

    got no clue where to start

--- calcalator.js
+++ calcalator.js
@@ -1,1 +1,12 @@
-//node extension pack and code runner
+document.write("1 2 3");
+document.write(" 4 5 6");
+document.write(" 7 8 9");
+document.write(" 0 + -");
+document.write(" * / =");
+
+function Caculator(math) {
+    for (let n = number; n % 4;)
+        if () {
+
+        }
+}
