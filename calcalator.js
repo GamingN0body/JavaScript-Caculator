658547f8897058a7e9dd2395ef2892c19e1c5282
HLoghan <LegoSaber06@gmail.com>
2024-01-25 13:40:41 -0800

    yep

--- calcalator.js
+++ calcalator.js
@@ -1,67 +1,38 @@
-//node extension pack and code runner
-let num = Number;
-function visuals() {
-    let text;
-    let n1 = prompt("1 2 3");
-    let n2 = prompt("4 5 6");
-    let n3 = prompt("7 8 9");
-    let n4 = prompt("0 + -");
-    let n5 = prompt("x / =");
-    if (dontknow) {
-        document.write(n1, n2, n3, n4, n5);
+function math() {
+    let n1 = prompt("Input number");
+    let sign = prompt("Input sign");
+    let n2 = prompt("Input number");
+
+    n1 = pauseFloat(n1);
+    n2 = pauseFloat(n2);
+
+    if (n1 === 0 || isNaN(n1)){
+        alert("Error 11111: Rereash page due to wrong input.");
+    }
+
+    if (n2 === 0 || isNaN(n2)){
+        alert("Error 11111: Rereash page due to wrong input.");
+    }
+
+    if (sign === "+") {
+
+    }
+
+    if (sign === "-") {
+
+    }
+
+    if (sign === "/") {
+
+    }
+
+    if (sign === "X" || "x" || "*") {
+
     }
 }
 
-let input = prompt("Please enter a caculation here.");
-function add(x, num) {
-        let input = prompt(x-num)
-        let output = document.write(x - num); 
-        
-        {document.write(add(x,num));}
-}
+
+
 
 //https://www.geeksforgeeks.org/javascript-program-to-make-a-simple-calculator/
 
-function minus(n, x) {
-    if (x === undefined) 
-    { return -x; }
-    else { return x - num; }
-}
-
-
-
-function multi(num) {
-    let input = promt("h/k=x")
-}
-
-
-function div(num) {
-    let input = promt(num + num)
-}
-
-
-function equal(num) {
-
-}
-
-
-function ifNeeded() {
-
-}
-
-
-console.log(visuals);
-console.log(minus);
-console.log(minus);
-console.log(multi);
-console.log(div);
-console.log(equal);
-console.log(ifNeeded);
-
-/*
-document.write(add);
-document.write(sub);
-document.write(multi);
-document.write(div);
-document.write(equal);
-document.write(ifNeeded);*/
