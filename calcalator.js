79db485ed450f093c0664d144a82fd0894617d80
HLoghan <LegoSaber06@gmail.com>
2024-01-25 14:45:22 -0800

    done

--- calcalator.js
+++ calcalator.js
@@ -1,38 +1,35 @@
-function math() {
-    let n1 = prompt("Input number");
+function math() { //to see the code put "math()" in the console
+    let n1 = prompt("Input number"); //will have a popup sayin input
     let sign = prompt("Input sign");
     let n2 = prompt("Input number");
 
-    n1 = pauseFloat(n1);
-    n2 = pauseFloat(n2);
-
-    if (n1 === 0 || isNaN(n1)){
+    if (n1 === 0 || isNaN(n1)){ //if input 1 is not a # then it will convert to the alert
         alert("Error 11111: Rereash page due to wrong input.");
     }
 
-    if (n2 === 0 || isNaN(n2)){
+    if (n2 === 0 || isNaN(n2)){ //if input 2 is not a # then it will convert to the alert
         alert("Error 11111: Rereash page due to wrong input.");
     }
 
-    if (sign === "+") {
+    n1 = parseFloat(n1); //will make it so that the + portain will confunction instead of it adding the 2 # like 1+1 =11 it will instead allow it to function as expected 1+1=2
 
+    n2 = parseFloat(n2); 
+    if (sign === "+") { 
+        sum = n1 + n2;
     }
 
-    if (sign === "-") {
-
+    else if (sign === "-") {
+        sum = n1 - n2;
     }
 
-    if (sign === "/") {
-
+    else if (sign === "/") {
+        sum = n1 / n2;
     }
 
-    if (sign === "X" || "x" || "*") {
-
+    else if (sign.toLowerCase() === "x" || "*") { //.toLowerCase will make any letter convert to be lower case
+        sum = n1 * n2;
     }
+    document.open; // this allows the answer to be seen on the screen
+    document.write(sum);
+    document.close;
 }
-
-
-
-
-//https://www.geeksforgeeks.org/javascript-program-to-make-a-simple-calculator/
-
